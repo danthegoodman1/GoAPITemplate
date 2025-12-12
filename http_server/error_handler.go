3b1c86b189bfce9a4171da9282ea414cc8167ea5
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-12-12 10:46:16 -0800

    handle interface

--- http_server/error_handler.go
+++ http_server/error_handler.go
@@ -2,15 +2,25 @@ package http_server
 
 import (
 	"errors"
+	"fmt"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog"
-	"net/http"
 )
 
 func customHTTPErrorHandler(err error, c echo.Context) {
 	var he *echo.HTTPError
 	if errors.As(err, &he) {
-		c.String(he.Code, he.Message.(string))
+		var message string
+		if he.Message == nil {
+			message = http.StatusText(he.Code)
+		} else if msg, ok := he.Message.(string); ok {
+			message = msg
+		} else {
+			message = fmt.Sprint(he.Message)
+		}
+		c.String(he.Code, message)
 		return
 	}
 
